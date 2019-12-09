81b02d86111ce38f59bcfda5655bab388afcd5c6
Erick <erickluques@gmail.com>
2019-12-09 01:55:01 -0200

    Example config

---
+++ src/database-config.php
@@ -0,0 +1,12 @@
+<?php
+//example
+/*
+return [
+    'host' => '<server>',
+    'db_user' => '<user>',
+    'db_password' => '<password>',
+    'db_name' => '<database_name>',
+    'db_driver' => 'mysql',
+    'charset' => 'utf-8'
+];
+*/
